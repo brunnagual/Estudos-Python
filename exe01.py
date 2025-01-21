296aabe7aafaeff75cc172c07374f8824c4201d3
Brunna Gualberto <109802322+brunnagual@users.noreply.github.com>
2025-01-21 17:55:32 -0300

    Adicionando comentarios

--- exe01.py
+++ exe01.py
@@ -1,7 +1,12 @@
+#Exercicio de Entrevista.
+
+#Pega inputs do usuário
 nome = input('Qual o seu nome? ')
 idade = int(input('Qual a sua idade? '))
+#Crio variáveis para facilitar o entendimento.
 num_letras = len(nome)
 idade_mais_velho = int(idade) + 5
+#Exibo os resultados do código
 print('Oi ' + nome)
 print('Seu nome tem {} letras.'.format(num_letras))
 print('Daqui 5 anos você terá {} anos.'.format(idade_mais_velho))
